5fef86378f6c0e91a238554ad6b32c6370db1ccb
Drew Stoutin <drewstoutin97@gmail.com>
2022-01-27 13:34:48 -0600

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SystemsDesignConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
